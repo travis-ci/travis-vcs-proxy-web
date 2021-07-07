75e68aa4989c43c04215cba4154453c26d178f98
AndriiMysko <usandrewmisko@gmail.com>
2021-07-07 13:11:57 +0300

    Add show password icon

--- app/components/sign-in.js
+++ app/components/sign-in.js
@@ -9,6 +9,24 @@ export default class SignIn extends Component {
   @service auth;
 
   @action
+  togglePassword() {
+    let element = document.getElementById('password');
+    if (element.getAttribute('type') === 'password') {
+      element.setAttribute('autocomplete', 'off');
+      element.setAttribute('autocorrect', 'off');
+      element.setAttribute('spellcheck', 'off');
+      element.setAttribute('autocapitalize', 'off');
+      element.setAttribute('type', 'text');
+    } else if (element.getAttribute('type') === 'text') {
+      element.removeAttribute('autocomplete');
+      element.removeAttribute('autocorrect');
+      element.removeAttribute('spellcheck');
+      element.removeAttribute('autocapitalize');
+      element.setAttribute('type', 'password');
+    }
+  }
+
+  @action
   signIn() {
     this.auth.signIn(this.email, this.password);
   }
