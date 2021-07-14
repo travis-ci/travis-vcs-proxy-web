75a5c5d054c15a144b1297b93a9de01cd8d93dc3
Hero <sergiue@bk.ru>
2021-07-14 15:42:40 +0300

    First page without confirmation, Forgot password, 2FA

--- app/router.js
+++ app/router.js
@@ -10,4 +10,9 @@ Router.map(function () {
   this.route('index', { path: '/' });
   this.route('sign-in', { path: '/sign_in' });
   this.route('sign-up', { path: '/sign_up' });
+  this.route('two-factor-authentication', { path: '/two_factor_authentication' });
+  this.route('setup-new-password', { path: '/setup_new_password' });
+  this.route('forgot-password', { path: '/forgot_password' });
+  this.route('first-page-without-confirmation', { path: '/first_page_without_confirmation' });
+
 });
