b51d6d6ec9c60d1f09937096d68dce66c15dbb6c
AndriiMysko <usandrewmisko@gmail.com>
2021-09-20 13:25:09 +0300

    Fix repo model missing api

--- app/models/repository.js
+++ app/models/repository.js
@@ -1,9 +1,10 @@
-import Model, { attr, belongsTo } from '@ember-data/model';
+import Model, { attr } from '@ember-data/model';
 import { inject as service } from '@ember/service';
 
 export default class RepositoryModel extends Model {
   @service storage;
   @service store;
+  @service api;
 
   @attr('string') name;
   @attr('string') url;
